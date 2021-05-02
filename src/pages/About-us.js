f94353b63f6486eebe6238b4ed6129655adf9f1f
sperezc06 <sperezc06@gmail.com>
2021-05-02 17:29:12 -0500

    New Origin part

--- src/pages/About-us.js
+++ src/pages/About-us.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import AboutUs_Slider from '../components/AboutUs-slider/AboutUs-slider';
-import Info_org from '../components/Info-org/Info_org';
+import React from "react";
+import AboutUs_Slider from "../components/AboutUs-slider/AboutUs-slider";
+import Info_org from "../components/Info-org/Info_org";
+import Origin from "../components/Origin/Origin";
 
-const About_us = () =>{
-  return(
+const About_us = () => {
+  return (
     <React.Fragment>
       <AboutUs_Slider />
       <Info_org />
+      <Origin />
     </React.Fragment>
   );
-}
+};
 export default About_us;
