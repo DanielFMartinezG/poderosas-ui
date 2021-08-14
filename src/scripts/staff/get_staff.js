e3f13f4ffce77e306873b2cd5ae5581303423fab
DanielMG <danielfmartinezg@outlook.com>
2021-08-13 23:13:16 -0500

    add function to staffArray to call the poderosas staff from the API

--- src/scripts/staff/get_staff.js
+++ src/scripts/staff/get_staff.js
@@ -1,26 +1,9 @@
-let staff_array = ()=> {
-  let staff_array = [
-    {
-      img:'https://media-exp1.licdn.com/dms/image/C4D03AQEF2iY1ChdRNw/profile-displayphoto-shrink_400_400/0/1590353556639?e=1626307200&v=beta&t=6_rm2QJKsH4refCLlzyc5ll8VP4pDrHyGJce4vP7ls0',
-      name: 'mariana sanz de santamaria',
-      position: 'fundadora',
-    },
-    {
-      img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3paJrZvZytip0h6-YwJFuV9GexAG4ui8I7A&usqp=CAU',
-      name: 'persona de ejemplo',
-      position: 'ejemplo',
-    },
-    {
-      img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSCNeYaXFI0JkDP2frmwBqwWCR5QXgARChWPQ&usqp=CAU',
-      name: 'persona 2 de ejemplo',
-      position: 'ejemplo',
-    },
-    {
-      img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSCNeYaXFI0JkDP2frmwBqwWCR5QXgARChWPQ&usqp=CAU',
-      name: 'persona 3 de ejemplo',
-      position: 'ejemplo',
-    },
-  ]
-  return staff_array;
+const routh = 'http://localhost:3000';
+
+async function staff_array () {
+  
+  const response = await fetch(`${routh}/staff/getStaff`);
+  const images = await response.json(); 
+  return images.resultado;
 }
 export default staff_array;
