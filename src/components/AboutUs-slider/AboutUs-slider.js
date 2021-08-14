8f72a4b3da534b15717c37e5d7830222f4586d04
DanielMG <danielfmartinezg@outlook.com>
2021-08-13 22:31:32 -0500

    add useState to control the images in the about us's slider, execute function to get call the images from the API

--- src/components/AboutUs-slider/AboutUs-slider.js
+++ src/components/AboutUs-slider/AboutUs-slider.js
@@ -2,22 +2,23 @@ import React, { useState, useEffect } from 'react'
 import btn_left from '../../assets/img-stickers/slide-bttn-left.png'
 import btn_right from '../../assets/img-stickers/slide-bttn-right.png'
 import AbourUs_slider_img from './AboutUs-slider-img'
-import get_slider_imgs from '../../scripts/aboutus-slider/get_aboutus_slider_img'
+import getSliderImgs from '../../scripts/aboutus-slider/get_aboutus_slider_img'
 import slide_images from '../../scripts/aboutus-slider/slide_images'
 import change_aboutUs_img from '../../scripts/aboutus-slider/change_aboutus_img'
 import '../../styles/aboutUs_slider.css'
 
 const AboutUs_Slider = () => {
-  // const[key_number, set_key_number] = useState(0);
-  let images_array = get_slider_imgs();
-  let key_number = 0;
-  let change_key = () => {
-    key_number++;
-    return key_number;
-  }
-  useEffect(() => {
-    slide_images();
-  });
+
+  const [imagesArray, setImagesArray] = useState([]);
+
+  useEffect(async function () {
+    const response = await getSliderImgs();
+    setImagesArray(response);
+    if(response.length > 3){
+      slide_images();
+    }
+  }, []
+  );
   return (
     <section className="about-us-slider-section" id="about-us-slider-section">
       <h2 className='main-title'>Somos Poderosas</h2>
@@ -28,10 +29,10 @@ const AboutUs_Slider = () => {
         <div className="about-us-slide-box">
           <div className="about-us-slide">
             {
-              images_array.map(image => (
+              imagesArray.map(image => (
                 <AbourUs_slider_img
                   url_img={image.img_src}
-                  key={`slide-img-${(change_key())}`}
+                  key={image._id}
                 />
               ))
             }
