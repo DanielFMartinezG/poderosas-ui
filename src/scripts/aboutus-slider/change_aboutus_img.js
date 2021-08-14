eab9faa466029072e8b58894a72f5e08b06317a1
DanielMG <danielfmartinezg@outlook.com>
2021-08-13 23:01:30 -0500

    add conditional in about us slider, that idea is that this function work when the slide had 3 images minimum

--- src/scripts/aboutus-slider/change_aboutus_img.js
+++ src/scripts/aboutus-slider/change_aboutus_img.js
@@ -2,10 +2,12 @@ import slide_images from './slide_images'
 let change_aboutUs_img = (event) =>{
   let img_box_slide = document.getElementsByClassName("about-us-img-box");
   //identificamos mediante el event si se activo el botón derecho o izquierdo
-  if(event.target.id == 'bttn-slider-right'){
-    slide_images(0,"bttn_right");
-  }else if(event.target.id == 'bttn-slider-left'){
-    slide_images(img_box_slide.length-1,"bttn_left");
+  if(img_box_slide.length > 3){
+    if(event.target.id == 'bttn-slider-right'){
+      slide_images(0,"bttn_right");
+    }else if(event.target.id == 'bttn-slider-left'){
+      slide_images(img_box_slide.length-1,"bttn_left");
+    }
   }
 }
 export default change_aboutUs_img;
