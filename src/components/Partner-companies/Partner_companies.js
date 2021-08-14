deff84a59a3dc4a52208105bc839ad75c94606e0
DanielMG <danielfmartinezg@outlook.com>
2021-08-13 22:57:54 -0500

    add function in useEffect to call the partner companies from the API, this response are going to save in the usesState

--- src/components/Partner-companies/Partner_companies.js
+++ src/components/Partner-companies/Partner_companies.js
@@ -1,21 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import companies_array from '../../scripts/partner-companies/get_companies';
 import '../../styles/partner_companies.css';
 import Companie_img from './Companie_img';
-const Parner_companies = () =>{
-  const companies_imgs = companies_array();
-  return(
+const Parner_companies = () => {
+
+  const [companies, setCompanies] = useState([]);
+  useEffect(async function () {
+    setCompanies(await companies_array());
+  }, []);
+
+  return (
     <section className="partner-companies-section">
       <h2 className="main-title">Empresas Aliadas</h2>
       <div className="partner-companies">
         <div className="partner-companies-container">
-          {companies_imgs.map((companie)=>(
-            <Companie_img 
-              url_img = {companie.img_src}
+          {companies.map((companie) => (
+            <Companie_img
+              url_img={companie.img_src}
               key={companie.companie_name.replace(/\s+/g, '')}
             />
           ))
-        }
+          }
         </div>
       </div>
     </section>
